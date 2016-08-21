de089b751b7e3035926d549a22814a0c14a43827
Nicholas Gallardo <nic.gallardo@gmail.com>
2016-08-21 11:42:49 -0600

    update mobile view off

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,20 +1,42 @@
 angular.module('portfolioApp', ["ngRoute"])
+
   .controller("HomeCtrl", function ($scope){
+
+    $scope.master = {};
+
     setTimeout(function(){
       document.getElementById('home-input').style.visibility = "visible";
       $(".custom-input").focus();
-      setTimeout(function(){
-        console.log('i ran after the vis appeared')
-      }, 2000)
-    },12700);
+    },10000);
+
+    $scope.update = function(visitor) {
+      $scope.master = angular.copy(visitor);
+      phaseOut(visitor.name);
+    };
+
+    const phaseOut = function(name){
+      $(".home-interactive").fadeOut(1000, function() { $(this).remove(); })
+      $( "#greeting-text" ).append(`Well 'ello there ${name}!`);
+      setTimeout(()=>{
+        document.getElementById('greeting').style.visibility = "visible";
+        $("#greeting-text").addClass('interactive');
+      },2000)
+    }
+
 
   })
+
   .controller("AboutCtrl", function($scope, $location){
     $scope.path = $location.path()
   })
+
   .controller("PortfolioCtrl", function($scope, $location){
     $scope.path = $location.path()
   })
+
+
+
+
   .config(function($routeProvider, $locationProvider){
     $routeProvider
       .when('/', {
